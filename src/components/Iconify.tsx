bf5cc1eabf2c8beda420989f2b74083aa59a4586
malik-korucu <malik.korucu@dugun.com>
2022-10-22 18:42:01 +0300

    Table component completed and slot structer added

--- src/components/Iconify.tsx
+++ src/components/Iconify.tsx
@@ -1,26 +1,21 @@
-import PropTypes from "prop-types";
-// icons
-import { Icon } from "@iconify/react";
-// @mui
-import { Box } from "@mui/material";
-import { FC, memo } from "react";
-
-// ----------------------------------------------------------------------
+import { Icon } from '@iconify/react'
+import { Box } from '@mui/material'
+import { FC, memo } from 'react'
 
 type Props = {
-  icon: any;
-  sx?: any;
-  color?: string;
-  width?: number;
-  height?: number;
-};
+  icon: any
+  sx?: any
+  color?: string
+  width?: number
+  height?: number
+}
 
 const Iconify: FC<Props> = (props) => {
-  const { icon, sx, color, ...other } = props;
+  const { icon, sx, color, ...other } = props
 
   return (
     <Box component={Icon} color={color} icon={icon} sx={{ ...sx }} {...other} />
-  );
-};
+  )
+}
 
-export default memo(Iconify);
+export default memo(Iconify)
